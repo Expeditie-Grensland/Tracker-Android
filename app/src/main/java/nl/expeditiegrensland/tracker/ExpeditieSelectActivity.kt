12281c88ac81b5d1aea770bcf92108ee756a9fd4
Martijn Atema <martijn@atema.one>
2018-10-17 18:22:38 +0200

    Empty Expeditie Select activity

--- app/src/main/java/nl/expeditiegrensland/tracker/ExpeditieSelectActivity.kt
+++ app/src/main/java/nl/expeditiegrensland/tracker/ExpeditieSelectActivity.kt
@@ -1,8 +1,7 @@
 package nl.expeditiegrensland.tracker
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_expeditie_select.*
+import android.support.v7.app.AppCompatActivity
 import nl.expeditiegrensland.tracker.types.Expeditie
 
 class ExpeditieSelectActivity : AppCompatActivity() {
@@ -13,14 +12,5 @@ class ExpeditieSelectActivity : AppCompatActivity() {
         val expedities = intent.getParcelableArrayListExtra<Expeditie>(Constants.BUNDLE_KEY_EXPEDITIES)
 
         setContentView(R.layout.activity_expeditie_select)
-
-//        var text = expedities.toString()
-//
-//        if (expedities != null)
-//            for (expeditie in expedities) {
-//                text += expeditie.name
-//            }
-
-        textView.text = expedities.toString()
     }
 }
