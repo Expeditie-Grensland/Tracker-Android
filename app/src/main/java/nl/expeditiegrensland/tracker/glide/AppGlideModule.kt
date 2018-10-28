85411c841148255ff4d8fbb476b4a00deaea61fe
Martijn Atema <martijn@atema.one>
2018-10-28 22:06:02 +0100

    Use Glide to retrieve (& cache) images

---
+++ app/src/main/java/nl/expeditiegrensland/tracker/glide/AppGlideModule.kt
@@ -0,0 +1,7 @@
+package nl.expeditiegrensland.tracker.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppGlideModule : AppGlideModule()
--- app/src/main/java/nl/expeditiegrensland/tracker/layout/expeditieselect/ExpeditieCardRecyclerViewAdapter.kt
+++ app/src/main/java/nl/expeditiegrensland/tracker/layout/expeditieselect/ExpeditieCardRecyclerViewAdapter.kt
@@ -1,14 +1,17 @@
 package nl.expeditiegrensland.tracker.layout.expeditieselect
 
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import kotlinx.android.synthetic.main.fragment_expeditie_card.view.*
 import nl.expeditiegrensland.tracker.Constants
 import nl.expeditiegrensland.tracker.R
-import nl.expeditiegrensland.tracker.helpers.DownloadImageTask
+import nl.expeditiegrensland.tracker.glide.GlideApp
 import nl.expeditiegrensland.tracker.types.Expeditie
 
 
@@ -29,10 +32,12 @@ class ExpeditieCardRecyclerViewAdapter(private val expedities: List<Expeditie>,
         with(holder.view) {
             name.text = item.name
             subtitle.text = item.subtitle
-            image.setBackgroundColor(Color.parseColor(item.color))
 
-            DownloadImageTask(image).execute(Constants.BASE_URL + item.image)
-
+            GlideApp.with(this)
+                    .load(Constants.BASE_URL + item.image)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .placeholder(ColorDrawable(Color.parseColor(item.color)))
+                    .into(image)
 
             setOnClickListener {
                 mListener(item)
